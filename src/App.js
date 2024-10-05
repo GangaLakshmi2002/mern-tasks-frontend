60a457864ff11a31d55a068d5e1709ad179137e4
GangaLakshmi2002 <gangalakshmi.mudda2002@gmail.com>
2024-10-05 16:11:27 +0530

    users list removed

--- src/App.js
+++ src/App.js
@@ -7,23 +7,22 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Dashboard from './components/Dashboard';
 import TaskForm from './components/TaskForm';
-import UserList from './components/UserList';
+// import UserList from './components/UserList';
 import EditTask from './components/EditTask';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <h2 className="text-3xl font-bold text-center">TASKS APP</h2>
+    <div className="min-h-screen bg-gray-100 pb-2">
+      <h1 className="text-4xl font-semibold text-center text-gray-100 bg-blue-400 p-2">TASKS APP</h1>
       <BrowserRouter>
         <Routes>
         <Route element={<Navigate to="login" />} path="/" />
           <Route path='/login' element={<Login />} />
-          {/* <Route path='/' element={<Login />} /> */}
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path="/register" element={<Register />} />
           <Route path='/verify-email/:token' element={<VerifyEmail />} />
           <Route path='/create-task' element={<TaskForm />} />
-          <Route path='/users' element={<UserList />} />
+          {/* <Route path='/users' element={<UserList />} /> */}
           <Route path='/edit-task/:id' element={<EditTask />} />
 
           
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,9 +5,9 @@ import TaskList from './TaskList'
 const Dashboard = () => {
   return (
     <div className='max-w-68xl mx-auto mt-6 p-6 shadow-md rounded-md bg-gray-200'>
-        <h2 className='text-2xl font-bold mb-4'>Dashboard</h2>
+        <h2 className='text-2xl font-bold mb-4 text-center items-center'>Dashboard</h2>
         <button className=' justify-end mb-4 bg-blue-500 p-2 mr-9  text-white hover:bg-blue-600 rounded-md'><Link to={"/create-task"}>Create Task</Link></button>
-        <button className=' justify-end mb-4 bg-blue-500 p-2  text-white hover:bg-blue-600 rounded-md'><Link to={"/users"}>users</Link></button>
+        {/* <button className=' justify-end mb-4 bg-blue-500 p-2  text-white hover:bg-blue-600 rounded-md'><Link to={"/users"}>users</Link></button> */}
 
         <TaskList />
 
